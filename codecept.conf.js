211618dbff6c06815699bf5ece6c8fdb5028fafd
Taimoor Rashid <114182214+Taimoor2500@users.noreply.github.com>
2022-10-29 00:49:50 +0500

    Add files via upload

---
+++ codecept.conf.js
@@ -0,0 +1,52 @@
+exports.config = {
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'https://demo.reactstorefront.io/',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  timeout: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.js']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    retryFailedStep: {
+      enabled: true
+    },
+    retryTo: {
+      enabled: true
+    },
+    eachElement: {
+      enabled: true
+    },
+    pauseOnFail: {}
+  },
+  stepTimeout: 0,
+  stepTimeoutOverride: [{
+      pattern: 'wait.*',
+      timeout: 0
+    },
+    {
+      pattern: 'amOnPage',
+      timeout: 0
+    }
+  ],
+  tests: './*_test.js',
+  name: 'codecept'
+}
